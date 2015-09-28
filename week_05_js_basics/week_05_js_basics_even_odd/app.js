88a26f657f61906e9b2bd3a6ff976238b0b65377
jasonpaulso <jason@jasonpaulsouthwell.com>
2015-09-27 18:25:41 -0700

    Week 05

---
+++ week_05_js_basics/week_05_js_basics_even_odd/app.js
@@ -0,0 +1,34 @@
+// In-Class Lab / Homework: Part 1
+
+// Write a for loop that will iterate from 0 to 20.
+// For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+// Do this using a FOR loop and then try to replicate the same functionality with a WHILE loop.
+// You will have to look up the syntax for the while loop, and also the modulus (%) operator.
+
+for (var i = 0; i <=20; i++) {
+    if (i > 0 && i % 2 == 0){
+    	alert(i + " is even!");
+    };
+};
+
+// var i = 0;
+
+// while (i < 20) {
+// 	i++;
+// 	alert(x + " is even");
+// }
+
+// // var i = 0;
+ 
+// // do {
+// //     i++;
+// //     alert(i + " is even");
+// // } while (i < 10 && i % 2 ==0);
+
+// // var count = 0;
+        
+// // while (count < 10) {
+// //     document.writeln("looping away!");
+ 
+// //     count++;
+// // }
---
+++ week_05_js_basics/week_05_js_basics_max/app.js
@@ -0,0 +1,17 @@
+
+// function max(num1, num2) {
+// 	if (num1 > num2) {
+// 		return num1;
+// 	} else {
+// 		return num2;
+// 	}
+// }
+
+
+// console.log(max(6,2))
+
+function maxOfThree (num1, num2, num3) {
+	 return (Math.max(num1, num2, num3))
+}
+
+console.log(maxOfThree(50,2,10))
